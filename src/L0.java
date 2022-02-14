cafabedbdd714f31a24c74ae97d92cfd39cd055f
Artem Tabachenko <fexo5000@icloud.com>
2022-02-14 17:36:24 +0300

    Без обработок ошибок в main

--- src/L0.java
+++ src/L0.java
@@ -1,26 +1,32 @@
 import java.util.*;
+import java.io.*;
 
-class Engine{
+//Exception - класс, который сигнализирует о сообщении об ошибке.
+//new Exception ("message")
+//throw - ключевое слово, которое позволяет выбросить ошибку.
+//throws SomeExceptionType - конструкция, которая говорит о возможных
+//исключениях в методе.
+//ResultType methodName (...) throws Exception
+
+class Engine {
     private Random r;
+
     Engine() { r = new Random(); }
-    int start() {
-        if (r.nextInt(10)== 2) {
-            System.out.println("Engine has broken");
-            return 1;
+    void start() throws Exception {
+        if (r.nextInt(10) == 2) {
+            throw new Exception("Engine has broken");
         }
         System.out.println("Started engine");
-        return 0;
     }
-    int get() {
-        if (r.nextInt(10)== 2) {
-            System.out.println("Engine has broken");
-            return 1;
+    int get() throws Exception {
+        if (r.nextInt(10) == 2) {
+            throw new Exception("Engine has broken in get");
         }
         return r.nextInt(1000);
     }
 }
 
-class Car{
+class Car {
     private Engine engine;
     private Random r;
 
@@ -28,33 +34,22 @@ class Car{
         this.engine = engine;
         this.r = new Random();
     }
-    int move() {
-       int engineOk = engine.start();
-       if (engineOk == 0) {
-           if (r.nextInt(4) == 1) {
-               System.out.println("Too much snow");
-               return 2;
-           }
-           int n = engine.get();
-           if (n < 0) {
-               return n;
-           }
-           int speed = n / 100;
-           System.out.println("Mooving car with speed " + speed);
-           return 0;
-       } else {
-           return engineOk;
+    void move() throws Exception {
+       engine.start();
+       if (r.nextInt(4) == 1) {
+           throw new Exception("Too much snow");
        }
+       int speed = engine.get() / 100;
+       System.out.println("Moving car with speed " + speed);
     }
 }
 
 public class L0 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Car car = new Car(new Engine());
         for (int i=0; i<30; i++){
             System.out.println("=== " + i + " ===");
-            int moveOk = car.move();
-            System.out.println(moveOk);
+            car.move();
         }
     }
 }
